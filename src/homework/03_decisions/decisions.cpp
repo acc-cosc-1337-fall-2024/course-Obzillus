4f72d7a74bd0ff8eea49ee6232e311f25d88adb1
Obzillus <106827173+Obzillus@users.noreply.github.com>
2024-09-19 00:12:30 +0000

    added the letter grade functions and a little bit of main

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -2,3 +2,58 @@
 
 
 //Write code for function(s) code here
+char get_letter_grade_using_if(int grade)
+{
+    char letter;
+
+    if(grade <= 100 && grade >= 90)
+    {
+        letter = 'A';
+    }
+    else if(grade <= 89 && grade >= 80)
+    {
+        letter = 'B';
+    }
+    else if(grade <= 79 && grade >= 70)
+    {
+        letter = 'C';
+    }
+    else if(grade <= 69 && grade >= 60)
+    {
+        letter = 'D';
+    }
+    else if(grade <= 59 && grade >= 0)
+    {
+        letter = 'F';
+    }
+    
+    return letter;
+}
+
+char get_letter_grade_using_switch(int grade)
+{
+    char letter;   
+    switch (grade)
+    {
+    case 90 ... 100:
+        letter = 'A';
+        break;
+    case 80 ... 89:
+        letter = 'B';
+        break;
+    case 70 ... 79:
+        letter = 'C';
+        break;
+    case 60 ... 69:
+        letter = 'D';
+        break;
+    case 0 ... 59:
+        letter = 'F';
+        break;
+    
+    default:
+        break;
+    }
+    
+    return letter;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,24 @@
 //write include statements
+#include <iostream>
+#include "decisions.h"
+
+using std::cout, std::cin;
 
 int main() 
 {
+	cout
+	<<"    MAIN MENU \n"
+	<<"1-Ltter grade using if \n"
+	<<"2-Letter grade using switch \n"
+	<<"3-Exit";
+
+	int input;
+	cin>>input;
+
+	if(input != 3)
+	{
+		int inputed_grade;
+	}
+
 	return 0;
 }
