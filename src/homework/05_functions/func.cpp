ec1cdf6925723324aa3cb9d5ff7edf8f0b3409c3
Obzillus <106827173+Obzillus@users.noreply.github.com>
2024-10-01 15:10:00 +0000

    H@ 5 h file and part of cpp file

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,23 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+double get_gc_content(const string& dna)
+{
+    double sum = 0;
+    for (auto i = 0; i < dna.length(); i++)
+    {
+        if(dna[i] == 'G' || dna[i] == 'C') sum++;
+    }
+    return sum/dna.length();
+}
+string get_dna_complement(string dna)
+{
+    for (auto i = 0; i < dna.length(); i++)
+    {
+        if(dna[i] = 'C') dna[i] = 'G';
+        if(dna[i] = 'G') dna[i] = 'C';
+
+        if(dna[i] = 'A') dna[i] = 'T';
+        if(dna[i] = 'T') dna[i] = 'A';
+    }
+}
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,7 @@
+#include<string>
+
+using std::string;
 //add function(s) prototype here
+double get_gc_content(const string& dna);
+string get_dna_complement(string dna);
+string reverse_string(string dna);
